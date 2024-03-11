f7e562bb6269d382538172eb10588bb186efe504
DESKTOP-9L1CUIA\lenovo <sudipsojitra2509@gmail.com>
2024-03-11 19:57:53 +0530

    add media query and index at front

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 // toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section');
@@ -24,4 +30,9 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links(scrolls)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
 }
