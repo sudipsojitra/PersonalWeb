17869215b8dc9caab78a877f2306e84755d82f4c
DESKTOP-9L1CUIA\lenovo <sudipsojitra2509@gmail.com>
2024-03-13 19:53:02 +0530

    complete demo without orignal content

--- script.js
+++ script.js
@@ -24,7 +24,15 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
         }
+        // if want to use animation that repeatson scroll use this
+        else {
+            sec.classList.remove('show-animate');
+        }
+
+
     });
     // sticky header
     let header = document.querySelector('header');
@@ -35,4 +43,7 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', (this.innerHeight + this.scrollY + 1) >= document.scrollingElement.scrollHeight);
 }
